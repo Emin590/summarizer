29aa4939d9762e21b5645a0445bf1b4da51b1981
emin <demirtasemin136@gmail.com>
2025-10-27 17:15:18 +0100

    day 1 - working on input where link goes

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-navbar p-4 flex justify-left'>
+        <h1 className='flex items-center' >
+            Summarizer.com
+        </h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ components/Title.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <>
+      <h1 className='text-2xl font-bold mb-4 text-center sm:text-3xl md:text-4xl lg:text-5xl'>
+        Welcome to Summarizer.com
+      </h1>
+      <p className='text-base text-center sm:text-md md:text-lg lg:text-xl max-w-2xl'>
+        Save time by getting your YouTube video summarized by link
+      </p>
+    </>
+  )
+}
+
+export default Title
