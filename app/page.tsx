354539cb229fd80bed1320b1cb08a77627b9c7f7
emin <demirtasemin136@gmail.com>
2025-10-27 17:41:54 +0100

    day 1.2 last save of the day

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Title from '@/components/Title';
 import { Input } from '@/components/ui/input';
+import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
     <div className='flex flex-col min-h-screen items-center bg-navbar gap-8 pt-20 px-4 sm:px-6 lg:px-8'>
       <Title />
-      <Input />
-    </div>
+      <div className='flex flex-row gap-2 w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl'>
+        <Input type="text" placeholder="Enter link here" className="bg-white border-none"/>
+        <Button variant="outline" className="bg-button border-none hover:bg-white">Summarize</Button>
+      </div>
+
+    </div>  
   )
 }
 
